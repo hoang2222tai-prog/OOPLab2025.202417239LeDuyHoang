009cdb339f7131b7b2f690e45c8a0648f8f6b7e9
hoang2222tai-prog <hoang2222tai@gmail.com>
2026-04-14 16:32:13 +0700

    Update Cart.java

--- src/Cart.java
+++ src/Cart.java
@@ -4,29 +4,29 @@ public class Cart{
     private int count=0;
 
     public void add(DigitalVideoDisc d){
-        public void add(DigitalVideoDisc d1,DigitalVideoDisc d2){
-    add(d1);
-    add(d2);
-}
-
-public void add(DigitalVideoDisc[] ds){
-    for(int i=0;i<ds.length;i++){
-        add(ds[i]);
-    }
-}
-
-public void add(DigitalVideoDisc... ds){
-    for(int i=0;i<ds.length;i++){
-        add(ds[i]);
-    }
-}
-
         if(count<MAX){
             list[count]=d;
             count++;
             System.out.println("da them");
         }else{
             System.out.println("day roi");
+        }
+    }
+
+    public void add(DigitalVideoDisc d1,DigitalVideoDisc d2){
+        add(d1);
+        add(d2);
+    }
+
+    public void add(DigitalVideoDisc[] ds){
+        for(int i=0;i<ds.length;i++){
+            add(ds[i]);
+        }
+    }
+
+    public void add(DigitalVideoDisc... ds){
+        for(int i=0;i<ds.length;i++){
+            add(ds[i]);
         }
     }
 
@@ -50,5 +50,19 @@ public void add(DigitalVideoDisc... ds){
             tong+=list[i].getGia();
         }
         return tong;
+    }
+
+    public void in(){
+        System.out.println("gio hang:");
+        for(int i=0;i<count;i++){
+            System.out.println(list[i].toString());
+        }
+    }
+
+    public DigitalVideoDisc tim(int id){
+        for(int i=0;i<count;i++){
+            if(list[i].getId()==id) return list[i];
+        }
+        return null;
     }
 }
